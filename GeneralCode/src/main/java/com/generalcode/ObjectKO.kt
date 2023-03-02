f0f47dcd6c0039aa1b14dd8a7c0032eff5c64242
janardanborda <janardan.borada@syphnosys.com>
2023-03-02 16:36:05 +0530

    V-0.0.3

---
+++ GeneralCode/src/main/java/com/generalcode/ObjectKO.kt
@@ -0,0 +1,5 @@
+package com.generalcode
+
+object ObjectKO {
+
+}
---
+++ GeneralCode/src/main/java/com/generalcode/TestToastKo.kt
@@ -0,0 +1,10 @@
+package com.generalcode
+
+import android.content.Context
+import android.widget.Toast
+
+class TestToastKo {
+    fun showToast(context: Context?, message: String?) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+}
